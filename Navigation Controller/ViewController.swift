d8f9bc1f069d51adbc3947168efdc366d762b9d7
forloop <yendheakshayd@gmail.com>
2023-02-03 11:14:27 +0530

    Navigation Controller

--- Navigation Controller/ViewController.swift
+++ Navigation Controller/ViewController.swift
@@ -11,9 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //self.title = "My Title"
+    }
+    
+    @IBAction func didTapButton(){
+        let vc = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        navigationController?.pushViewController(vc, animated: true)
+        vc.navigationItem.largeTitleDisplayMode = .never
+    }
+
+}
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Hello"
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapButton(){
+        navigationController?.popViewController(animated: true)
     }
 
 
 }
+
 
